99b5fb028c1fec54f595885c71da3342a966a42c
ajeetjain7 <ajjain688@gmail.com>
2026-06-16 10:46:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.post(
 );
 
 app.use(cors(
-    {  origin : "http://localhost:5173" ,
+    {  origin : "https://one-examnoteaiclient.onrender.com" ,
         credentials : true,
         methods : ["GET" , "POST" , "PUT" , "DELETE" , "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
